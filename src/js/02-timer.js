96da62d5494a4a866f114d42339fc896ba94f914
gabrielg-goit <151449282+gabrielg-goit@users.noreply.github.com>
2024-05-05 16:23:20 +0300

    three

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,65 +1,116 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+//For style
+const timerContainer = document.querySelector('.timer');
+timerContainer.style.display = 'flex';
+timerContainer.style.flexDirection = 'row';
+timerContainer.style.alignItems = 'center';
+timerContainer.style.gap = '25px';
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
+const fieldContainer = document.querySelectorAll('.field');
+for (const container of fieldContainer) {
+  container.style.display = 'flex';
+  container.style.flexDirection = 'column';
+  container.style.alignItems = 'center';
+  container.style.justifyContent = 'center';
 }
 
-const startButton = document.querySelector('[data-start]');
-const datetimePicker = document.querySelector('#datetime-picker');
+const valuesSpace = document.querySelectorAll('.value');
+for (const values of valuesSpace) {
+  values.style.font = 'bold 30px roboto,serif';
+}
 
-flatpickr('#datetime-picker', {
+const detailSpace = document.querySelectorAll('.label');
+for (const detail of detailSpace) {
+  detail.style.font = '15px roboto,serif';
+  detail.style.textTransform = 'uppercase';
+}
+
+const options = {
   enableTime: true,
   time_24hr: true,
+  defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    const selectedDate = selectedDates[0];
-    if (selectedDate > new Date()) {
-      startButton.disabled = false;
+    console.log('Val:', selectedDates[0]);
+  },
+};
+flatpickr('#datetime-picker', options);
+
+//Identification for dynamic elements
+const inputData = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMinutes = document.querySelector('[data-minutes]');
+const dataSeconds = document.querySelector('[data-seconds]');
+
+//disable initial Start button
+startBtn.disabled = true;
+
+//enable Start button on valid input
+function validateInput() {
+  inputData.addEventListener('change', () => {
+    const minData = new Date();
+    console.log(minData);
+    const selectedData = new Date(inputData.value);
+    console.log(selectedData);
+    if (selectedData >= minData) {
+      startBtn.disabled = false; //disable buton start
     } else {
       Notiflix.Notify.failure('Please choose a date in the future');
     }
-    console.log(selectedDates[0]);
-  },
-});
-
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
-const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]');
-
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
+  });
 }
+validateInput();
 
-startButton.addEventListener('click', functionStart);
-function functionStart() {
-  const selectedDate = new Date(datetimePicker.value).getTime();
-  const currentTime = new Date().getTime();
-  const timeDifference = selectedDate - currentTime;
-  if (timeDifference <= 0) {
-    return;
-  }
-  const intervalID = setInterval(() => {
-    const remainingTime = convertMs(selectedDate - new Date().getTime());
-    days.textContent = addLeadingZero(remainingTime.days);
-    hours.textContent = addLeadingZero(remainingTime.hours);
-    minutes.textContent = addLeadingZero(remainingTime.minutes);
-    seconds.textContent = addLeadingZero(remainingTime.seconds);
-    if (selectedDate - new Date().getTime() <= 0) {
-      clearInterval(intervalID);
+//Start behaviour
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true; //disable  start  button
+
+  const timerId = setInterval(() => {
+    // Number of milliseconds per unit of time
+    const targetDate = new Date(inputData.value);
+
+    // Get the current date and time
+    const currentDate = new Date();
+
+    // Calculate the time difference between the target date and the current date
+    const timeDifference = targetDate.getTime() - currentDate.getTime();
+
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(timeDifference / day);
+    // Remaining hours
+    const hours = Math.floor((timeDifference % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((timeDifference % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor(
+      (((timeDifference % day) % hour) % minute) / second
+    );
+
+    console.log(days, hours, minutes, seconds); //de verificare
+    //return { days, hours, minutes, seconds };
+
+    // For stop countdown at 0ms
+    if (timeDifference < 1000) {
+      clearInterval(timerId);
+      console.log('Timp expirat'); //de verificare
     }
+
+    // Add 0 function at start of number if length < 2
+    const leadingZero = value => value.toString().padStart(2, '0');
+
+    dataDays.textContent = leadingZero(days);
+    dataHours.textContent = leadingZero(hours);
+    dataMinutes.textContent = leadingZero(minutes);
+    dataSeconds.textContent = leadingZero(seconds);
   }, 1000);
-  startButton.disabled = true;
-}
+});
